bc6be6d9d05a74b5fa605f91d839d9b2c5104d02
Jan Novopacky <jan.novopacky@gmail.com>
2023-05-30 11:30:19 +0200

    add redis to vat

--- vat_new.py
+++ vat_new.py
@@ -1,5 +1,26 @@
+import os
 import requests
-from dataclasses import dataclass
+import json
+import redis
+import logging
+
+from dataclasses import dataclass, asdict
+
+
+logging.basicConfig(
+    format="[%(asctime)s +0000] [%(process)d] [%(levelname)s] %(message)s",
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+
+def default_headers():
+    return {
+        'content-type': 'application/x-www-form-urlencoded',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
+    }
+
+
 
 @dataclass
 class Company:
@@ -11,13 +32,26 @@ class Company:
     vatNumber: str = None
 
 
+redis_host_name = os.environ.get("REDIS_HOST_NAME", "localhost")
+
+@dataclass
 class VatInfo:
-    def __init__(self, vat_input: str):
-        self.base_url = 'https://pexpats.com/api/v1/util-functions/get-vat-info'
-        self.country = vat_input[:2]
-        self.vat_number = vat_input[2:]
+    vat_input: str
+    base_url: str = 'https://pexpats.com/api/v1/util-functions/get-vat-info'
+
+    def __post_init__(self):
+        self.country = self.vat_input[:2]
+        self.vat_number = self.vat_input[2:]
+        self.redis_db = redis.Redis(host=redis_host_name, port=6379, db=0)
 
     def get_vat_info(self):
+        redis_key = f'vat_info:{self.country}:{self.vat_number}'
+        cached_data = self.redis_db.get(redis_key)
+        
+        if cached_data is not None:
+            logging.info("Used cache data!")
+            return Company(**json.loads(cached_data))
+
         headers = {
             'Accept': 'application/json',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
@@ -27,12 +61,11 @@ class VatInfo:
             'vatNumber': self.vat_number
         }
         response = requests.get(self.base_url, headers=headers, params=params)
+
         if response.status_code == 200:
             data = response.json()
-            # Extract only the needed properties
             properties = data.get('properties', {})
             address = properties.get('address')
-            # Replace newline characters with a space
             if address is not None:
                 address = address.replace('\n', ' ')
             company = Company(
@@ -43,8 +76,13 @@ class VatInfo:
                 address=address,
                 vatNumber=properties.get('vatNumber')
             )
+
+            if company.isValid:
+                self.redis_db.setex(redis_key, 7 * 24 * 60 * 60, json.dumps(asdict(company)))
+                return company
+
             return company
-        else:
-            return None
+
+        return None
 
 
